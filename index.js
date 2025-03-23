1fa4b535e224aaa1ef1d67ccf7ffb45a26df71a9
Adil <115704572+Ash4477@users.noreply.github.com>
2025-03-24 03:34:46 +0500

    changed to local db version

--- index.js
+++ index.js
@@ -1,106 +1,54 @@
 import http from "http";
 import url from "url";
-import process from "process";
-import mongoose, { mongo } from "mongoose";
-import mongodb from "mongodb";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
-const pokeList = [
-  {
-    pokeName: "metapod",
-    pokeImageUrl:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/11.png",
-  },
-  {
-    pokeName: "butterfree",
-    pokeImageUrl:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/12.png",
-  },
-  {
-    pokeName: "weedle",
-    pokeImageUrl:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/13.png",
-  },
-  {
-    pokeName: "kakuna",
-    pokeImageUrl:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/14.png",
-  },
-  {
-    pokeName: "beedrill",
-    pokeImageUrl:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/15.png",
-  },
-  {
-    pokeName: "pidgey",
-    pokeImageUrl:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/16.png",
-  },
-  {
-    pokeName: "pidgeotto",
-    pokeImageUrl:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/17.png",
-  },
-  {
-    pokeName: "pidgeot",
-    pokeImageUrl:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/18.png",
-  },
-  {
-    pokeName: "rattata",
-    pokeImageUrl:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/19.png",
-  },
-  {
-    pokeName: "raticate",
-    pokeImageUrl:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/20.png",
-  },
-];
-
-// Connection to Mongoose
-const client = new mongodb.MongoClient(process.env.MONGO_URI, {
-  serverApi: {
-    version: mongodb.ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-async function connectDB() {
+// Connect to Local MongoDB using Mongoose
+const connectDB = async () => {
   try {
-    await client.connect();
-    console.log("✅ Connected to MongoDB!");
+    await mongoose.connect("mongodb://localhost:27017/pokemon_data");
+    console.log("✅ MongoDB Connected (Local)");
   } catch (error) {
     console.error("❌ MongoDB Connection Error:", error);
     process.exit(1);
   }
-}
+};
 
-await connectDB();
-
-const db = client.db("pokemon_data");
-const pokemonCollection = db.collection("pokemons");
-
-// Server
-const server = http.createServer(async (req, res) => {
-  const queryObj = url.parse(req.url, true).query;
-  const limit = Number(queryObj.limit) || 5;
-
-  const pokemonList = await pokemonCollection
-    .find({})
-    .limit(limit)
-    .project({ pokeName: 1, pokeImageUrl: 1, _id: 0 })
-    .toArray();
-
-  res.writeHead(200, {
-    "content-type": "application/json",
-    "access-control-allow-origin": "*",
-  });
-  res.write(JSON.stringify(pokemonList));
-  res.end();
+// Define Mongoose Schema & Model
+const pokeSchema = new mongoose.Schema({
+  pokeName: { type: String, required: true },
+  pokeImageUrl: { type: String, required: true },
 });
 
-server.listen(8080, () => console.log("Server listening..."));
+const Pokemon = mongoose.model("Pokemon", pokeSchema);
+
+// Start Server After DB Connection
+connectDB().then(() => {
+  const server = http.createServer(async (req, res) => {
+    const queryObj = url.parse(req.url, true).query;
+    const limit = Number(queryObj.limit) || 5;
+
+    try {
+      const pokemonList = await Pokemon.find({})
+        .limit(limit)
+        .select("pokeName pokeImageUrl -_id");
+
+      res.writeHead(200, {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      });
+      res.write(JSON.stringify(pokemonList));
+      res.end();
+    } catch (err) {
+      console.error("❌ Database Query Error:", err);
+      res.writeHead(500, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ error: "Internal Server Error" }));
+    }
+  });
+
+  server.listen(8080, () =>
+    console.log("🚀 Server running on http://localhost:8080")
+  );
+});
